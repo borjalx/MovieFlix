a9cf5648e8cf6e3d42d187f3618a9bd7009aa17e
Borja S <borjas@marc-roig.local>
2019-01-30 15:21:07 +0100

    Initial Commit

---
+++ MovieFlix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieFlix
+//
+//  Created by Borja S on 30/01/2019.
+//  Copyright © 2019 Borja S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
